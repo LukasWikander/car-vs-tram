37e1dd577fbaf5dcaff2c06bffdfed6d33919b35
Lukas Wikander <lukas.wikander@gmail.com>
2018-07-17 12:08:52 +0200

    Created an outer function for running the assignment given a set of parameters. Also made constraint noncompliance throw an error.

--- Assignment Combined Project/init_assignment.m
+++ Assignment Combined Project/init_assignment.m
@@ -1,7 +1,4 @@
-
-clear all
-close all
-clc
+function outputs = init_assignment(general_params,tram_params,car_params,doPlot)
 
 %% Load variables, initialise
 drv_mission  = load ('dc_AtoB.mat');
@@ -15,76 +12,8 @@ a = M*drv_mission.dc.slope;
 drv_mission.dc.pos = cumsum([0; l.*cos(a)]); % Flight distance [m]
 drv_mission.dc.alt = cumsum([drv_mission.dc.altinit; l.*sin(a)]);
 
-%% Assignment given parameters
-general_params = struct;
-general_params.m_pass_kg = 80; % Passenger weight [kg]
-general_params.c_E_kWh = 1; % Cost of energy [SEK/kWh]
-general_params.v_max_kmh = 70; % Maximum allowed velocity [km/h]
-general_params.c_batt_kWh = 1000; % Cost of battery [SEK/kWh]
-general_params.e_batt_kWh = 0.1; % Energy density of battery [kWh/kg]
-general_params.c_motor_kW = 70+50; % Cost of motor + cost of power electronics [SEK/kW]
-general_params.C_rate = 1.5; % Charge C rate [A/Ah]
-general_params.c_station_kW = 2000; % Charge station cost [SEK/kW]
-general_params.c_stop = 3.3*1e6; % Tram or car stop (station) [SEK/location]
-general_params.n_variations = 10; % Number of tram variations (grid size)
-
-%% Assumed additional parameters
-general_params.V_batt = 400;	% Battery pack voltage [V]
 general_params.l_trip = drv_mission.dc.s(end); % Length of trip [m]
 general_params.l_round_trip = 2*drv_mission.dc.s(end); % Length of round trip [m]
-general_params.c_tram_track_km = 112e6; % Cost of track [SEK/km]
-general_params.c_tram_track_mnt_yr = (368+172)/5/249*10*1e6; % Cost of track yearly when in use [SEK/km/year]
-general_params.l_life_tram_yr = 40; % Lifetime of tram [year]
-general_params.l_life_car_km = 160000; % Expected mileage of car [km]
-general_params.t_zerotomax = 20; % Acceleration time for constructed drive cycle [s]
-
-general_params.rho_air = 1.1839;	% Air density [kg/m3]
-general_params.acc_gravity = 9.81;	% Acceleration of gravity [m/s2]
-
-%% Tram vehicle parameters
-tram_params = struct;
-tram_params.name = 'tram_full';
-tram_params.m_kg = 36800; % Tram weight [kg]
-tram_params.n_pass = 85 + 117; % Seated + standing passenger capacity 
-tram_params.Cd = 1.8; % Tram drag coefficient
-tram_params.Ad = 3.32*(1.435+0.4); % Tram frontal area
-tram_params.Cr = 0.001; % Tram wheel rolling resistance
-tram_params.rw = 0.2;	% Tram wheel radius
-tram_params.t_unload = 2 * 60; % Time at stops [s]
-tram_params.c_purchase = 1.08*37600000/18*11.75 + 3500 + 48000; % Purchase cost of tram + gear box + AD components [SEK]
-tram_params.c_maintenance = 1.3; % Maintenance cost of tram + track per km driven [SEK/km]
-tram_params.c_parking = (67.5/7)*1e6; % Parking cost (tram depot) [SEK/vehicle] (Not realistic, probably ~ 40 + 4*n_trams)
-tram_params.v_max_kmh = 70; % Maximum allowed velocity [km/h] (see assignment)
-tram_params.P_max_kW = inf; % Maximum power [kW]
-
-% Dynamic programming cost function parameters:
-tram_params.accpenalty = 0.25;
-tram_params.traveltimepenalty = 0.25*0.058;
-tram_params.energypenalty = 0.1;
-
-%% Car vehicle parameters
-car_params = struct;
-car_params.name = 'car_full';
-car_params.m_kg = 2500; % Car weight [kg]
-car_params.n_pass = 10;	% Passenger capacity
-car_params.Cd = 0.5; % Car drag coefficent
-car_params.Ad = 1.79*1.9; % Car frontal area
-car_params.rw = 0.37;	% Car wheel radius
-car_params.Cr = 0.02;	% Car rolling resistance
-car_params.t_unload = 5*60;   % Time at stops [s]
-car_params.c_purchase = 300000+3500+48000; % Purchase cost of car (base + gear box + AD components) [SEK]
-car_params.c_maintenance = 0.277; % Maintenance cost of car per km driven [SEK/km]
-car_params.c_parking = 0; % Parking cost [SEK/vehicle] (TODO)
-car_params.v_max_kmh = 70; % Maximum allowed velocity [km/h] (see assignment)
-car_params.P_max_kW = inf; % Maximum power [kW]
-
-% Dynamic programming cost function parameters:
-car_params.accpenalty = 0.25;
-car_params.traveltimepenalty = 0.058;
-car_params.energypenalty = 0.1;
-
-% NOTE: Increased battery size (compared to trip traction energy) to 
-% decrease charging time per trip
 
 %% Vehicle simulation
 % Full tram
@@ -130,15 +59,35 @@ if any(fleet_info_output.fleet_constraint_compliance == 0)
 	n_noncompliant_cars = sum(fleet_info_output.car_constraint_compliance == 0);
 	n_noncompliant_trams = sum(fleet_info_output.tram_constraint_compliance == 0);
 	if n_noncompliant_cars
-		warning(['Infeasible with selected car battery size for ' num2str(n_noncompliant_cars) ' configurations.'])
+		throw(MException('INIT_ASSIGNMENT:InfeasibleCarBattery',...
+			['Infeasible with selected car battery size for ' num2str(n_noncompliant_cars) ' configurations.']));
 	end
 	if n_noncompliant_trams
-		warning(['Infeasible with selected tram battery size for ' num2str(n_noncompliant_trams) ' configurations.'])
+		throw(MException('INIT_ASSIGNMENT:InfeasibleTramBattery',...
+			['Infeasible with selected tram battery size for ' num2str(n_noncompliant_trams) ' configurations.']));
 	end
 	
 end
+
 %% Plots
 % TODO: Plots
-plot_assignment(tram_params, car_params, general_params, drv_mission, pass_flow, tram_full_sim_output, car_full_sim_output, fleet_info_output, cost_estimation_output);
+if doPlot
+	plot_assignment(tram_params, car_params, general_params, drv_mission, pass_flow, tram_full_sim_output, car_full_sim_output, fleet_info_output, cost_estimation_output);
+end
 
 fprintf('Done!\n');
+
+%% Output
+outputs.params.tram = tram_params;
+outputs.params.car = car_params;
+outputs.params.general = general_params;
+outputs.params.drv_mission = drv_mission;
+outputs.params.pass_flow = pass_flow;
+outputs.results.tram_full = tram_full_sim_output;
+outputs.results.tram_empty = tram_empty_sim_output;
+outputs.results.car_full = car_full_sim_output;
+outputs.results.car_empty = car_empty_sim_output;
+outputs.results.fleet = fleet_info_output;
+outputs.results.costs = cost_estimation_output;
+
+end
--- Assignment Combined Project/main_assignment.m
+++ Assignment Combined Project/main_assignment.m
@@ -1,7 +1,89 @@
-function [outputArg1,outputArg2] = main_assignment(inputArg1,inputArg2)
-%MAIN_ASSIGNMENT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
+
+clear all
+close all
+clc
+
+
+%% Assignment given parameters
+general_params = struct;
+general_params.m_pass_kg = 80; % Passenger weight [kg]
+general_params.c_E_kWh = 1; % Cost of energy [SEK/kWh]
+general_params.v_max_kmh = 70; % Maximum allowed velocity [km/h]
+general_params.c_batt_kWh = 1000; % Cost of battery [SEK/kWh]
+general_params.e_batt_kWh = 0.1; % Energy density of battery [kWh/kg]
+general_params.c_motor_kW = 70+50; % Cost of motor + cost of power electronics [SEK/kW]
+general_params.C_rate = 1.5; % Charge C rate [A/Ah]
+general_params.c_station_kW = 2000; % Charge station cost [SEK/kW]
+general_params.c_stop = 3.3*1e6; % Tram or car stop (station) [SEK/location]
+general_params.n_variations = 10; % Number of tram variations (grid size)
+
+%% Assumed additional parameters
+general_params.V_batt = 400;	% Battery pack voltage [V]
+general_params.c_tram_track_km = 112e6; % Cost of track [SEK/km]
+general_params.c_tram_track_mnt_yr = (368+172)/5/249*10*1e6; % Cost of track yearly when in use [SEK/km/year]
+general_params.l_life_tram_yr = 40; % Lifetime of tram [year]
+general_params.l_life_car_km = 160000; % Expected mileage of car [km]
+general_params.t_zerotomax = 20; % Acceleration time for constructed drive cycle [s]
+
+general_params.rho_air = 1.1839;	% Air density [kg/m3]
+general_params.acc_gravity = 9.81;	% Acceleration of gravity [m/s2]
+
+%% Tram vehicle parameters
+tram_params = struct;
+tram_params.name = 'tram_full';
+tram_params.m_kg = 36800; % Tram weight [kg]
+tram_params.n_pass = 85 + 117; % Seated + standing passenger capacity 
+tram_params.Cd = 1.8; % Tram drag coefficient
+tram_params.Ad = 3.32*(1.435+0.4); % Tram frontal area
+tram_params.Cr = 0.001; % Tram wheel rolling resistance
+tram_params.rw = 0.2;	% Tram wheel radius
+tram_params.t_unload = 2 * 60; % Time at stops [s]
+tram_params.c_purchase = 1.08*37600000/18*11.75 + 3500 + 48000; % Purchase cost of tram + gear box + AD components [SEK]
+tram_params.c_maintenance = 1.3; % Maintenance cost of tram + track per km driven [SEK/km]
+tram_params.c_parking = (67.5/7)*1e6; % Parking cost (tram depot) [SEK/vehicle] (Not realistic, probably ~ 40 + 4*n_trams)
+tram_params.v_max_kmh = 70; % Maximum allowed velocity [km/h] (see assignment)
+tram_params.P_max_kW = inf; % Maximum power [kW]
+
+% Dynamic programming cost function parameters:
+tram_params.accpenalty = 0.25;
+tram_params.traveltimepenalty = 0.25*0.058;
+tram_params.energypenalty = 0.1;
+
+%% Car vehicle parameters
+car_params = struct;
+car_params.name = 'car_full';
+car_params.m_kg = 2500; % Car weight [kg]
+car_params.n_pass = 10;	% Passenger capacity
+car_params.Cd = 0.5; % Car drag coefficent
+car_params.Ad = 1.79*1.9; % Car frontal area
+car_params.rw = 0.37;	% Car wheel radius
+car_params.Cr = 0.02;	% Car rolling resistance
+car_params.t_unload = 5*60;   % Time at stops [s]
+car_params.c_purchase = 300000+3500+48000; % Purchase cost of car (base + gear box + AD components) [SEK]
+car_params.c_maintenance = 0.277; % Maintenance cost of car per km driven [SEK/km]
+car_params.c_parking = 0; % Parking cost [SEK/vehicle] (TODO)
+car_params.v_max_kmh = 70; % Maximum allowed velocity [km/h] (see assignment)
+car_params.P_max_kW = inf; % Maximum power [kW]
+
+% Dynamic programming cost function parameters:
+car_params.accpenalty = 0.25;
+car_params.traveltimepenalty = 0.058;
+car_params.energypenalty = 0.1;
+
+%% Run simulation with provided parameters
+clc
+init_assignment(general_params,tram_params,car_params,true);
+
+
+%% Sensitivity analysis: general_params
+clc
+sensitivity = sensitivity_analysis(general_params,tram_params,car_params);
+
+
+
+
+
+
+
+
 
